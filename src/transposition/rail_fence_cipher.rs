df79796ce8ba059b0dd1b1c9cc11ade5b241415f
Sehny <msehnout@users.noreply.github.com>
2017-09-29 19:31:28 +0300

    Remove old solution

--- src/transposition/rail_fence_cipher.rs
+++ src/transposition/rail_fence_cipher.rs
@@ -41,8 +41,8 @@ impl SymmetricCipher<RailFenceCipherText, String> for RailFenceCipher {
         let msg_len = input.char_indices().count();
         let list = RfCoordinates::generate(self, msg_len);
         let mut state: Vec<(RfCoordinates, char)> = list.into_iter().zip(input.chars()).collect();
-        state.sort_by(|a,b| {
-            ((a.0).0).cmp(&(b.0).0)
+        state.sort_by(|&(RfCoordinates(a, _), _), &(RfCoordinates(b, _), _)| {
+            a.cmp(&b)
         });
         let cipher_text: String = state
             .into_iter()
@@ -56,51 +56,3 @@ impl SymmetricCipher<RailFenceCipherText, String> for RailFenceCipher {
     }
 }
 
-/*pub struct RailFenceCipherText {
-    state: Vec<Vec<char>>,
-}
-
-impl fmt::Debug for RailFenceCipherText {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "RailFenceCipher\n{:?}\n{:?}\n{:?}\n", self.state[0], self.state[1], self.state[2])
-    }
-}
-
-pub struct RailFenceCipher;
-
-impl Default for RailFenceCipher {
-    fn default() -> Self {
-        RailFenceCipher {}
-    }
-}
-
-impl SymmetricCipher<RailFenceCipherText, String> for RailFenceCipher {
-    fn encrypt(&self, input: &String) -> RailFenceCipherText {
-        let len = input.char_indices().count();
-        let mut state = vec![
-            std::iter::repeat('.')
-                .take(len)
-                .collect::<Vec<char>>(); 3];
-        let mut row = 0;
-        let mut step = 1;
-        for i in 0..len {
-            state[row][i] = input.chars().nth(i).unwrap();
-            row = (row as i32 + step) as usize;
-            if row == 2 {
-                step = -1;
-            } else if row == 0 {
-                step = 1;
-            }
-        }
-        RailFenceCipherText {
-            state
-        }
-    }
-
-    fn decrypt(&self, input: &RailFenceCipherText) -> String {
-        "ahoj".to_string()
-    }
-
-}
-
-*/
